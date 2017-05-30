9018c0a27f994adf6fb0a72294baef2ec870d78a
Foxel <thesprouti@gmail.com>
2017-05-30 20:02:38 +0300

    Cleanup
    - Removed useless stuff from MazeGenerator.
    - Removed lamps and other too specific assets and features.
    - Removed fade in and out textures.

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -6,14 +6,7 @@ public class MazeGenerator : MonoBehaviour
 {
 	public GameObject mazePrefab = null;
 
-    /// <summary>
-    /// List of materials to shade the beginning and end of the maze with.
-    /// </summary>
-	public List<Material> shadeMaterials = null;
-
-    /// <summary>
     /// Size of a room in world dimensions.
-    /// </summary>
 	public Vector2 roomDim;
 
     /*
@@ -24,23 +17,14 @@ public class MazeGenerator : MonoBehaviour
 	public GameObject straightFloor = null;
 	public GameObject tFloor = null;
 	public GameObject xFloor = null;
-    /*
-     * Wall prefabs.
-     */
+
+	/// Wall model prefab.
 	public GameObject wall = null;
-	public GameObject altWall = null;
-    /*
-     * Ceiling prefab.
-     */
+	/// Ceiling model prefab.
 	public GameObject ceiling = null;
 
-    // Lamp prefab.
-	public GameObject lamp = null;
-    // Index positions to place lamps at.
-	private List<Point> lampPositions;
-    // Interval to place lamps at (in rooms).
-	public int lampInterval = 5;
-	private int currentLampDistance = 0;
+	/// Default maze material.
+	public Material defaultMaterial = null;
 
     // End point prefab.
 	public GameObject endPoint = null;
@@ -99,15 +83,11 @@ public class MazeGenerator : MonoBehaviour
     /// <summary>
     /// Generates a maze with the given dimensions.
     /// </summary>
-    /// <param name="width"></param>
-    /// <param name="height"></param>
     /// <returns>A brand-new maze to play with.</returns>
 	public Maze GenerateMaze(int width, int height)
 	{
 		endPointDist = 0;
 		endPointCoord = new Point(-1, -1);
-
-		lampPositions = new List<Point>();
 
 		int[,] grid = new int[height, width];
         // Generate the maze.
@@ -126,7 +106,6 @@ public class MazeGenerator : MonoBehaviour
         // Create the visual parts for the maze.
 		maze.Initialise(width, height, roomDim);
 		CreateRooms(grid, maze);
-		CreateLamps(maze);
 
 		maze.AddEndPoint(endPointCoord, endPoint, Quaternion.Euler(0.0f, endPointRotation, 0.0f));
 
@@ -143,14 +122,6 @@ public class MazeGenerator : MonoBehaviour
 				Debug.Log("Weird starting room " + grid[0, 0]);
 				break;
 		}
-
-        // Shade the beginning of the maze to fade from dark to light.
-		Crawler.Crawl(maze, new Vector3(), Nav.GetFacing(maze.startRotation.y), shadeMaterials.Count - 1, SetRoomShade);
-        // Shade the ending of the maze to fade from light to dark.
-		Crawler.Crawl(maze, Nav.GetPosAt(endPointCoord, maze.roomDim), Nav.GetFacing(endPointRotation), shadeMaterials.Count - 1, SetRoomShade);
-
-        // Swap wall styles for every new maze.
-		useAltWall = !useAltWall;
 
 		return maze;
 	}
@@ -179,14 +150,6 @@ public class MazeGenerator : MonoBehaviour
             // Check that the new position is within the maze dimensions.
 			if ((ny >= 0 && ny < grid.GetLength(0)) && (nx >= 0 && nx < grid.GetLength(1)) && (grid[ny, nx] == 0))
 			{
-                // Spawn a lamp if one should be spawned here.
-				currentLampDistance++;
-				if (currentLampDistance >= lampInterval)
-				{
-					currentLampDistance = 0;
-					lampPositions.Add(new Point(nx, ny));
-				}
-
                 // Set the connection bits in this new room and the room we came from.
 				grid[y, x] |= Room.bits[dir];
 				grid[ny, nx] |= Room.bits[Nav.opposite[dir]];
@@ -196,15 +159,11 @@ public class MazeGenerator : MonoBehaviour
 			}
 		}
 
-		currentLampDistance = 0;
         // Set the end point here if its deeper in the maze than the previous one.
 		if (distance > endPointDist)
 		{
 			endPointCoord.Set(x, y);
 			endPointDist = distance;
-
-            // No lamps allowed on the end point.
-			lampPositions.Remove(new Point(x, y));
 
             // Check which way to face the end point in.
 			foreach (Dir dir in directions)
@@ -238,33 +197,23 @@ public class MazeGenerator : MonoBehaviour
                 // Create a floor for the room.
 				GameObject floor = CreateFloor(grid[y, x]);
 				floor.transform.SetParent(roomInstance.transform, false);
-				floor.GetComponent<MaterialSetter>().SetMaterial(shadeMaterials[0]);
+				floor.GetComponent<MaterialSetter>().SetMaterial(defaultMaterial);
 
                 // Create walls for the room.
 				GameObject walls = CreateWalls(grid[y, x]);
 				walls.transform.SetParent(roomInstance.transform, false);
-				walls.GetComponent<MaterialSetter>().SetMaterial(shadeMaterials[0]);
+				walls.GetComponent<MaterialSetter>().SetMaterial(defaultMaterial);
 
                 // Create a ceiling for the room.
 				GameObject ceiling = CreateCeiling();
 				ceiling.transform.SetParent(roomInstance.transform, false);
 				ceiling.transform.position += new Vector3(0.0f, 2.0f, 0.0f);
-				ceiling.GetComponent<MaterialSetter>().SetMaterial(shadeMaterials[0]);
+				ceiling.GetComponent<MaterialSetter>().SetMaterial(defaultMaterial);
 
                 // Create the actual room and add it to the Maze.
 				Room room = new Room(grid[y, x], new Point(x, y), roomInstance);
 				maze.AddRoom(room);
 			}
-		}
-	}
-
-	private void CreateLamps(Maze maze)
-	{
-		foreach (Point lampPos in lampPositions)
-		{
-			GameObject lampInstance = (GameObject)Instantiate(lamp, new Vector3(), Quaternion.identity);
-			lampInstance.name = "Lamp";
-			maze.AddItem(lampPos, lampInstance);
 		}
 	}
 
@@ -299,8 +248,7 @@ public class MazeGenerator : MonoBehaviour
             // If the room value has this direction bit set, create a wall.
 			if ((~value & Room.bits[dir]) > 0)
 			{
-				GameObject wallInstance = (GameObject)Instantiate((useAltWall) ? altWall : wall,
-						new Vector3(),
+				GameObject wallInstance = (GameObject)Instantiate(wall, new Vector3(),
 						Quaternion.Euler(0.0f, Nav.GetRotation(dir), 0.0f));
 				wallInstance.transform.SetParent(wallsInstance.transform, false);
 				wallInstance.transform.position += wallInstance.transform.rotation * new Vector3(-roomDim.y / 2.0f + wallInset, 0.0f, 0.0f);
@@ -317,20 +265,6 @@ public class MazeGenerator : MonoBehaviour
 					Quaternion.identity);
 		ceilingInstance.name = "Ceiling";
 		return ceilingInstance;
-	}
-
-    /// <summary>
-    /// Sets the shade of a Room based on its distance from the darkest point of shading.
-    /// </summary>
-    /// <param name="room">Room to set the shade of.</param>
-    /// <param name="distance">Distance of the room from the darkest point of shading.</param>
-	private void SetRoomShade(Room room, int distance)
-	{
-		room.instance.GetComponent<MaterialSetter>().SetMaterial(shadeMaterials[distance]);
-        // No lamps allowed in shaded areas.
-		Transform lamp = room.instance.transform.Find("Lamp");
-		if (lamp != null)
-			MonoBehaviour.Destroy(lamp.gameObject);
 	}
 
     /// <summary>
