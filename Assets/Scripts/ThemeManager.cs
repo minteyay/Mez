c5d37043ea2b17439c0e073595aed0a94cb37571
Foxel <thesprouti@gmail.com>
2017-07-28 17:52:04 +0300

    No more Windows holla holla :c

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -114,11 +114,10 @@ public class ThemeManager : MonoBehaviour
 		Material tilesetMaterial = new Material(defaultShader);
 		tilesetMaterial.mainTexture = tilesetTexture;
 
-		#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+	#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
 		// Turn Windows' backslashes into nice regular slashes.
 		tilesetPath = tilesetPath.Replace('\\', '/');
-		Debug.LogError("We on Windows holla holla~");
-		#endif
+	#endif
 
 		string tilesetName = tilesetPath.Substring(tilesetPath.LastIndexOf('/') + 1, tilesetPath.LastIndexOf(".png") - tilesetPath.LastIndexOf('/') - 1);
 		Tilesets.Add(tilesetName, tilesetMaterial);
