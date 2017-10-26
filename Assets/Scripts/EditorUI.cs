41a23d7f6ec2bfc05114877aa18f4e1df337f4f3
Foxel <thesprouti@gmail.com>
2017-10-26 22:47:42 +0300

    RoomStyle editor GUI

--- Assets/Scripts/EditorUI.cs
+++ Assets/Scripts/EditorUI.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class EditorUI : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class EditorUI : MonoBehaviour
 	[SerializeField] private InputField _mazeNameField = null;
 	[SerializeField] private InputField _mazeWidthField = null;
 	[SerializeField] private InputField _mazeHeightField = null;
+
+	[SerializeField] private GameObject _roomStyleList = null;
+	[SerializeField] private GameObject _roomStyleEntryPrefab = null;
+	private List<RoomStyleUI> _roomStyleEntries = new List<RoomStyleUI>();
 
 	private void Start()
 	{
@@ -35,14 +40,34 @@ public class EditorUI : MonoBehaviour
     private void ThemeLoaded()
     {
         _busyScreen.SetActive(false);
+		UpdateValues();
         GenerateMaze();
+    }
 
+	public void UpdateValues()
+	{
 		MazeRuleset ruleset = _themeManager.ruleset;
 
 		_mazeNameField.text = ruleset.name;
 		_mazeWidthField.text = ruleset.size.x.ToString();
 		_mazeHeightField.text = ruleset.size.y.ToString();
-    }
+
+		foreach (RoomStyleUI roomStyleEntry in _roomStyleEntries)
+			Destroy(roomStyleEntry.gameObject);
+		_roomStyleEntries.Clear();
+		
+		foreach (RoomStyle roomStyle in ruleset.roomStyles)
+		{
+			GameObject roomStyleEntry = Instantiate(_roomStyleEntryPrefab);
+			roomStyleEntry.transform.SetParent(_roomStyleList.transform);
+
+			RoomStyleUI roomStyleUI = roomStyleEntry.GetComponent<RoomStyleUI>();
+			roomStyleUI.roomStyle = roomStyle;
+			roomStyleUI.themeManager = _themeManager;
+			roomStyleUI.UpdateValues();
+			_roomStyleEntries.Add(roomStyleUI);
+		}
+	}
 
     public void GenerateMaze()
     {
@@ -63,5 +88,10 @@ public class EditorUI : MonoBehaviour
 		int height = Mathf.Max(1, int.Parse(newHeight));
 		_mazeHeightField.text = height.ToString();
 		_themeManager.ruleset.size.y = height;
+	}
+
+	public void RoomStyleNameChanged(int index, string newName)
+	{
+		_themeManager.ruleset.roomStyles[index].name = newName;
 	}
 }
---
+++ Assets/Scripts/RoomStyleUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class RoomStyleUI : MonoBehaviour
+{
+    [HideInInspector] public RoomStyle roomStyle = null;
+    [HideInInspector] public ThemeManager themeManager = null;
+
+    [SerializeField] private InputField _nameField = null;
+    [SerializeField] private Dropdown _tilesetDropdown = null;
+
+    public void UpdateValues()
+    {
+        _nameField.text = roomStyle.name;
+
+        _tilesetDropdown.ClearOptions();
+        string[] tilesets = new string[themeManager.textures.Count];
+        themeManager.textures.Keys.CopyTo(tilesets, 0);
+        _tilesetDropdown.AddOptions(new List<string>(tilesets));
+        for (int i = 0; i < tilesets.Length; i++)
+        {
+            if (tilesets[i] == roomStyle.tileset)
+            {
+                _tilesetDropdown.value = i;
+                break;
+            }
+        }
+    }
+
+    public void NameChanged(string newName)
+    {
+        roomStyle.name = newName;
+    }
+
+    public void TilesetChanged(System.Int32 index)
+    {
+        roomStyle.tileset = _tilesetDropdown.options[index].text;
+    }
+}
