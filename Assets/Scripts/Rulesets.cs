a89ed0aa44cade2226020ff1f78b159640bdbb78
Foxel <thesprouti@gmail.com>
2017-11-12 22:11:10 +0200

    Ruleset validation functions

--- Assets/Scripts/Rulesets.cs
+++ Assets/Scripts/Rulesets.cs
@@ -9,6 +9,19 @@ public class MazeRuleset
 
     public RoomStyle[] roomStyles;
     public RoomRuleset[] rooms;
+
+    public bool Validate(ThemeManager themeManager)
+    {
+        if (size.x <= 0 || size.y <= 0)
+            return false;
+        foreach (RoomStyle roomStyle in roomStyles)
+            if (!roomStyle.Validate(this, themeManager))
+                return false;
+        foreach (RoomRuleset room in rooms)
+            if (!room.Validate(this))
+                return false;
+        return true;
+    }
 }
 
 [System.Serializable]
@@ -18,6 +31,19 @@ public class RoomStyle
     public string tileset = "default";
 
     public DecorationRuleset[] decorations;
+
+    public bool Validate(MazeRuleset mazeRuleset, ThemeManager themeManager)
+    {
+        foreach (RoomStyle roomStyle in mazeRuleset.roomStyles)
+            if (roomStyle != this && roomStyle.name == name)
+                return false;
+        if (!themeManager.textures.ContainsKey(tileset))
+            return false;
+        foreach (DecorationRuleset decoration in decorations)
+            if (!decoration.Validate(themeManager))
+                return false;
+        return true;
+    }
 }
 
 [System.Serializable]
@@ -33,6 +59,26 @@ public class DecorationRuleset
 
     public bool TryParseChance(out float chance) { return float.TryParse(amount, out chance); }
     public bool TryParseCount(out Point countRange) { return Utils.TryParseRange(amount, out countRange); }
+
+    public bool Validate(ThemeManager themeManager)
+    {
+        if (!themeManager.textures.ContainsKey(texture))
+            return false;
+        switch (amountType)
+        {
+            case AmountType.Chance:
+                float chance;
+                if (!TryParseChance(out chance))
+                    return false;
+                break;
+            case AmountType.Count:
+                Point count;
+                if (!TryParseCount(out count))
+                    return false;
+                break;
+        }
+        return true;
+    }
 }
 
 [System.Serializable]
@@ -42,9 +88,25 @@ public class RoomRuleset
 
     public string style = "";
     public Start start = Start.Random;
-    public string count = "";
-    public string size = "";
+    public string count = 1.ToString();
+    public string size = 1.ToString();
 
     public bool TryParseCount(out Point countRange) { return Utils.TryParseRange(count, out countRange); }
     public bool TryParseSize(out Point sizeRange) { return Utils.TryParseRange(size, out sizeRange); }
+
+    public bool Validate(MazeRuleset mazeRuleset)
+    {
+        bool validStyle = false;
+        foreach (RoomStyle roomStyle in mazeRuleset.roomStyles)
+            if (roomStyle.name == style)
+                validStyle = true;
+        if (!validStyle)
+            return false;
+        Point range;
+        if (!TryParseCount(out range))
+            return false;
+        if (!TryParseSize(out range))
+            return false;
+        return true;
+    }
 }
