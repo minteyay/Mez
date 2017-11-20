678f9ba7839c68f8820f90f0d9635bf1188ee543
Foxel <thesprouti@gmail.com>
2017-11-20 17:25:12 +0200

    Range object

---
+++ Assets/Scripts/Utils/Range.cs
@@ -0,0 +1,14 @@
+﻿
+/// <summary>
+/// An integer range.
+/// </summary>
+[System.Serializable]
+public class Range : Point
+{
+	public override string ToString()
+	{
+		if (x == y)
+			return x.ToString();
+		return x + " - " + y;
+	}
+}
